07450ff201737c5e4dd499b0ecbbf39e13fd79b3
KaungthuKhant <kaungkhant@go.rmc.edu>
2022-01-02 22:10:47 -0500

    new Pawn object that will have all of pawn properties and functions

--- Pawn.java
+++ Pawn.java
@@ -1,3 +1,30 @@
 public class Pawn {
-    
+    private int color; // -1 for white and 1 for black
+    private boolean firstMove;
+    private int xPosition;
+    private int yPosition;
+
+    public Pawn(int aColor, int x, int y){
+        this.color = aColor;
+        this.firstMove = true;
+        this.xPosition = x;
+        this.yPosition = y;
+    }
+
+    public void move(int x, int y){
+        if (this.color == -1){
+            whiteMove();
+        }
+        else{
+            blackMove();
+        }
+    }
+
+    public void whiteMove(){
+
+    }
+
+    public void blackMove(){
+
+    }
 }
