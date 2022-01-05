f71bc45fb3073d2773029253910fbb3919487f4a
KaungthuKhant <kaungkhant@go.rmc.edu>
2022-01-05 14:11:59 -0500

    Pawn function done and starting another piece

--- Pawn.java
+++ Pawn.java
@@ -1,30 +1,59 @@
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.MoveAction;
+
 public class Pawn {
-    private int color; // -1 for white and 1 for black
+    private char color; // w for white and b for black
     private boolean firstMove;
     private int xPosition;
     private int yPosition;
 
-    public Pawn(int aColor, int x, int y){
+    public Pawn(char aColor, int x, int y){
         this.color = aColor;
         this.firstMove = true;
         this.xPosition = x;
         this.yPosition = y;
     }
 
-    public void move(int x, int y){
-        if (this.color == -1){
-            whiteMove();
+    // checkAndMove the pawn piece
+    public void checkAndMove(int x, int y){
+        boolean moveable = false;
+        // white
+        if (this.color == 'w'){
+            moveable = checkIfWhiteMoveIsValid(x, y);
         }
+        // black
         else{
-            blackMove();
+            moveable = checkIfBlackMoveIsValid(x, y);
+        }
+        
+        if (moveable){
+            this.xPosition = x;
+            this.yPosition = y;
         }
     }
 
-    public void whiteMove(){
-
+    public boolean checkIfWhiteMoveIsValid(int x, int y){
+        int xDifference = this.xPosition - x;
+        int yDifference = this.yPosition - y;
+        // check if the move is horizontally valid
+        if (xDifference < -1 || xDifference > 1){
+            return false;
+        }
+        // check if the move is vertically valid
+        if (!this.firstMove){
+            // needs to be positive in order to be valid
+            if (yDifference != 1){
+                return false;
+            }
+        }
+        else if(this.firstMove){
+            if (yDifference < 1 || yDifference > 2){
+                return false;
+            }
+        }
+        return true;
     }
 
-    public void blackMove(){
-
+    public boolean checkIfBlackMoveIsValid(int x, int y){
+        return false;
     }
 }
---
+++ board.java
@@ -0,0 +1,6 @@
+public class board {
+    
+
+    // check to make sure the next move is not a white piece
+    //public String checkColorOfSpot(x, y)
+}
