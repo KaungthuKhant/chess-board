ba55b63debc428c87046ef6e09bf1c4b0ef027dd
KaungthuKhant <kaungkhant@go.rmc.edu>
2022-01-05 15:25:53 -0500

    board started

--- board.java
+++ board.java
@@ -1,5 +1,6 @@
 public class board {
-    
+    private String[][] board;
+
     // the board is a 2 d array with each elements begin a spot
     // a spot contains: floorColor, posAphabet, posNumber
     // check to make sure the next move is not a white piece
