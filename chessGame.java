c1fcb4f86ff5e51d8a7fb1505c7e2a4ae17aa460
KaungthuKhant <kaungkhant@go.rmc.edu>
2022-01-05 21:40:08 -0500

    board decor

--- chessGame.java
+++ chessGame.java
@@ -1,4 +1,7 @@
+import javax.lang.model.type.PrimitiveType;
+
 public class chessGame {
+    public static char turn = 'w';
     public static void main(String[] args){
         String[][][] board = {
             {{"bP", "  "}, {"bP", "%%"}, {"bP", "  "}, {"bP", "%%"}, {"bP", "  "}, {"bP", "%%"}, {"bP", "  "}, {"bP", "%%"}},
@@ -10,5 +13,26 @@ public class chessGame {
             {{"wP", "  "}, {"wP", "%%"}, {"wP", "  "}, {"wP", "%%"}, {"wP", "  "}, {"wP", "%%"}, {"wP", "  "}, {"wP", "%%"}},
             {{"wR", "%%"}, {"wN", "  "}, {"wB", "%%"}, {"wQ", "  "}, {"wK", "%%"}, {"wB", "  "}, {"wN", "%%"}, {"wR", "  "}}
         };
+
+        int[] test = promptUser(board);
+    }
+
+    public static int[] promptUser(String[][][] board){
+        int[] moves = {0, 0};
+        if (turn == 'w'){
+            turn = 'b';
+            for (int i = 0; i < 8; i++){
+                System.out.println("-----------------------------------------");
+                System.out.print("| ");
+                for (int j = 0; j < 8; j++){
+                    System.out.print(board[i][j][0] + " | ");
+                }
+                System.out.println(" ");
+            }
+        }
+        else{
+            turn = 'w';
+        }
+        return moves;
     }
 }
